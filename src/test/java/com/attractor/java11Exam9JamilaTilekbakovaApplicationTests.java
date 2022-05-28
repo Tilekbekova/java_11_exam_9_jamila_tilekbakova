7e9385ff3e7b5015a230576c5ffb09f72f8afc01
TilekbakovaJamila <tilekbekovaja@gmail.com>
2022-05-28 07:05:12 +0300

    Создала проект

---
+++ src/test/java/com/attractor/java11Exam9JamilaTilekbakovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class java11Exam9JamilaTilekbakovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
